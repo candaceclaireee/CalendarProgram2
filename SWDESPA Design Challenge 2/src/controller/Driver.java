0bca977cedf997a6a697f2e7cfa3ab27fbe3d4d2
sairakaye <30943505+sairakaye@users.noreply.github.com>
2018-03-13 17:15:19 +0800

    Updates in Calendar Program FXML.

--- SWDESPA Design Challenge 2/src/controller/Driver.java
+++ SWDESPA Design Challenge 2/src/controller/Driver.java
@@ -15,6 +15,7 @@ public class Driver extends Application {
         Scene scene = new Scene(root);
         stage.setTitle("Productivity Tool");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
     
